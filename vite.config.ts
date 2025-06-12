0a5d97835afc911d00740608368514ed15236d92
Ugur Ata Dargun <76053601+uguratadargun@users.noreply.github.com>
2025-06-12 13:42:15 +0300

    Add local copy of livekit components

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@livekit/components-react': '/src/@livekit/components-react/dist',
+    },
+  },
   build: {
     // Performance optimizations
     target: "es2020",
