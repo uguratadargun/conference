2b8f59b5fa89fb2cff76593976c01218ad6d69d6
Ugur Ata Dargun <ugur.dargun@ordulu.com>
2025-06-30 11:46:00 +0300

    token

--- src/utils/livekit-token.ts
+++ src/utils/livekit-token.ts
@@ -4,7 +4,7 @@ const LIVEKIT_CONFIG = {
   apiKey: 'APIB6KUGSRtzEGw',
   apiSecret: 'bxKAKulJVpsHE0h2ehVGpRKp9zKBBRMOiFvfwZQz63K',
   projectName: 'ugurdargun-w5ph6ze0',
-  roomName: 'test3',
+  roomName: 'test61',
 } as const;
 
 export const generateToken = async (): Promise<{
@@ -16,14 +16,20 @@ export const generateToken = async (): Promise<{
     { length: 3 },
     () => chars[Math.floor(Math.random() * chars.length)]
   ).join('');
-  const username = `user_UGUR_${randomPart}`;
+  const username = `ugur_${randomPart}`;
 
   console.log(`Generated unique username: ${username}`);
 
   const at = new AccessToken(LIVEKIT_CONFIG.apiKey, LIVEKIT_CONFIG.apiSecret, {
     identity: username,
   });
-  at.addGrant({ roomJoin: true, room: LIVEKIT_CONFIG.roomName });
+  at.addGrant({
+    roomJoin: true,
+    room: LIVEKIT_CONFIG.roomName,
+    canSubscribe: false,
+    canPublish: false,
+    canPublishData: false,
+  });
   const token = await at.toJwt();
   const url = `ws://10.0.2.148:7880`;
 
